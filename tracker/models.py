57a3113117fc93b2618c6826e27ccf63e2113dd6
Anuja Agarwal <anujagrazzel@gmail.com>
2020-10-22 19:26:49 +0530

    Update models.py

--- tracker/models.py
+++ tracker/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Create your models here.
-exp_category=[ ('electronics', 'Electronics'),
+exp_category=[('electronics', 'Electronics'),
     ('fashion', 'Fashion'),
     ('kitchen', 'Kitchen'),
     ('pets', 'Pets'),
@@ -17,15 +17,16 @@ exp_category=[ ('electronics', 'Electronics'),
     ('other', 'Other')]
 
 class Expenses(models.Model):
-    date=models.DateTimeField(default=timezone.now)
-    name= models.CharField(max_length=50)
-    amount=models.IntegerField(default=0)
+    date = models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=50)
+    amount = models.IntegerField(default=0)
     exp_type = models.CharField(max_length=20, choices=exp_category, default='Other')
 
-    usr=models.ForeignKey(User, on_delete=models.CASCADE)
+    usr = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
-        return u"{} | {} | {} |  {} | {}".format(self.usr,self.name,self.amount,self.exp_type,self.date)
+        return u"{} | {} | {} |  {} | {}".
+        format(self.usr, self.name,self.amount, self.exp_type, self.date)
 
     def get_absolute_url(self):
         return reverse('index')
