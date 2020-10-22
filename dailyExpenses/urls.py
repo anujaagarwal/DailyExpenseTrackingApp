50af4c300221782017e1690915b9d0d15b0276a1
Anuja Agarwal <anujagrazzel@gmail.com>
2020-10-22 19:23:54 +0530

    Update urls.py

--- dailyExpenses/urls.py
+++ dailyExpenses/urls.py
@@ -22,8 +22,8 @@ from users import views as user_views
 #admin.autodiscover()
 urlpatterns = [
       url('', include('tracker.urls')),
-      url('register/',user_views.register,name='register'),
-    url('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
-       url('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
-    url(r'^admin/', admin.site.urls),
+      url('register/', user_views.register, name='register'),
+      url('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+      url('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+      url(r'^admin/', admin.site.urls),
 ]
